12b03246905550861e63a8633a5534aa69c513f0
Anthony E Lau <cs120wdb@ieng9.ucsd.edu>
2019-03-05 13:39:59 -0800

    Readded accidentally deleted test

--- pa4/t2.c
+++ pa4/t2.c
@@ -2,34 +2,29 @@
 #include "umix.h"
 #include "mycode4.h"
 
-#define NUMTHREADS 7
+#define NUMYIELDS	5
 
-/* Tests output of yield */
+/* Tests self yield */
 
-/* Tracks the yields as a comma separated list. src(->yld)->dest indicates
- * that thread src yielded to thread dest and yld was the return value of
- * the Yield call if control was exchange by an explicit yield.
+/* Tracks the yields as a comma separated list. src->dest indicates
+ * that thread src yielded to thread dest.
  */
 
 void Main() {
-    int yld;
     void doYieldStuff();
 
     InitThreads();
 
     Printf("->%d", GetThread());
-
-    for (int i = 1; i < NUMTHREADS; i++) {
-        CreateThread(doYieldStuff, i);
+    for (int i = 0; i < NUMYIELDS; i++) {
+        CreateThread(doYieldStuff, i + 1);
+        Printf(",%d", GetThread());
+        YieldThread(GetThread());
+        Printf("->%d", GetThread());
     }
-
     Printf(",%d", GetThread());
-    yld = YieldThread(GetThread());
-    Printf("->%d->%d", yld, GetThread());
-
-    Printf(",%d", GetThread());
-    yld = YieldThread(1);
-    Printf("->%d->%d", yld, GetThread());
+    YieldThread(1);
+    Printf("->%d", GetThread());
 
     Printf(",%d", GetThread());
     ExitThread();
@@ -37,16 +32,15 @@ void Main() {
 
 // t is this thread's id
 void doYieldStuff(int t) {
-    int yld;
     Printf("->%d", GetThread());
-
+    for (int i = 0; i < NUMYIELDS; i++) {
+        Printf(",%d", GetThread());
+        YieldThread(GetThread());
+        Printf("->%d", GetThread());
+    }
     Printf(",%d", GetThread());
-    yld = YieldThread(GetThread());
-    Printf("->%d->%d", yld, GetThread());
-
-    Printf(",%d", GetThread());
-    yld = YieldThread((t + 1) % NUMTHREADS);
-    Printf("->%d->%d", yld, GetThread());
+    YieldThread((t + 1) % (NUMYIELDS + 1));
+    Printf("->%d", GetThread());
 
     Printf(",%d", GetThread());
 }
